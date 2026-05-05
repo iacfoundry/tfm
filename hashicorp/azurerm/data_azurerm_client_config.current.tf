3391d6c14fa7c6ada1af9a3721b724936f2b7566
William Robinson <william.a.robinson@gmail.com>
2026-05-05 14:37:57 -0600

    Add azurerm_client_config

---
+++ hashicorp/azurerm/data_azurerm_client_config.current.tf
@@ -0,0 +1,7 @@
+# -----------------------------------------
+# Terraform Module
+# Contributor(s): William Robinson
+# Updated: March 2026
+# Purpose: Terraform Resource Module
+# -----------------------------------------
+data "azurerm_client_config" "current" {}
