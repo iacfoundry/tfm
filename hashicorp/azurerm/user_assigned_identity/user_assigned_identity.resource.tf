950799c9a1896b2c57efc733c9b8bfbfdb35b72c
William Robinson <william.a.robinson@gmail.com>
2026-05-05 17:28:30 -0600

    Updated formatting for resource and variables

--- hashicorp/azurerm/user_assigned_identity/user_assigned_identity.resource.tf
+++ hashicorp/azurerm/user_assigned_identity/user_assigned_identity.resource.tf
@@ -1,14 +1,14 @@
 resource "azurerm_user_assigned_identity" "user_assigned_identity" {
   for_each = { for key, value in var.azurerm_user_assigned_identity_data : key => value if value.enabled }
 
-# Required Arguments
-  location = each.value.location
-  name = each.value.name
+  # Required Arguments
+  location            = each.value.location
+  name                = each.value.name
   resource_group_name = each.value.resource_group_name
 
-# Optional Arguments
+  # Optional Arguments
   isolation_scope = each.value.isolation_scope
-  tags = each.value.tags
+  tags            = each.value.tags
 
 
 
--- hashicorp/azurerm/user_assigned_identity/user_assigned_identity.variables.tf
+++ hashicorp/azurerm/user_assigned_identity/user_assigned_identity.variables.tf
@@ -1,14 +1,14 @@
 variable "azurerm_user_assigned_identity_data" {
   type = map(object({
     # Required
-    enabled = bool
-    location = string
-    name = string
+    enabled             = bool
+    location            = string
+    name                = string
     resource_group_name = optional(string) # Set to Optional for allowed lookup by name value
-    
+
     # Optional
     isolation_scope = optional(string)
-    tags = optional(map(string))
+    tags            = optional(map(string))
   }))
   default = {}
 }
